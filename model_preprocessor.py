b22c85af4c1e9951a21e04d6f716a9f8a9a1fdc5
Анна <147938295+annamistery@users.noreply.github.com>
2026-01-24 16:08:13 +0300

    Add files via upload

--- model_preprocessor.py
+++ model_preprocessor.py
@@ -14,7 +14,7 @@ class ModelProcessor:
         genai.configure(api_key=self.api_key)
 
         # 3. Инициализируем модель
-        self.model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        self.model = genai.GenerativeModel('models/gemini-1.5-flash')
         self.prompt_path = "system_prompt.txt"
 
     def _load_system_instruction(self):
@@ -58,4 +58,3 @@ class ModelProcessor:
         with open(filename, "w", encoding="utf-8") as f:
             f.write(text)
         return filename
-
